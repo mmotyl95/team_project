878fd0e9f7179980fd0c589aa8259f77584482f6
kacper jakubek <kjakubek-ulan@taskrabbit.com>
2024-04-27 14:49:15 +0200

    Add new Timing view and delete search view.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "../views/Home.vue";
+import Timing from "../views/Timing.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +8,12 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: Home,
     },
     {
-      path: "/search",
-      name: "searchView",
-      component: () => import("../views/SearchView.vue"),
+      path: "/Timing",
+      name: "Timing",
+      component: Timing,
     },
   ],
 });
