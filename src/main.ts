abe799ff02231316bb52bd0e6219c271ccb86a1c
kacper jakubek <kjakubek-ulan@taskrabbit.com>
2024-04-27 14:49:18 +0200

    update npm packages and add internalization(i18n)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css"
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import i18n from './plugins/i18n'
 
 import App from "./App.vue";
 import router from "./router";
@@ -12,4 +13,5 @@ import router from "./router";
 createApp(App)
   .use(createPinia().use(piniaPluginPersistedstate))
   .use(router)
+  .use(i18n)
   .mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import basicSSL from "@vitejs/plugin-basic-ssl";
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import { dirname, resolve } from "node:path";
+
 
 // https://vitejs.dev/config/#conditional-config
 export default defineConfig(({ mode }) => {
@@ -35,6 +38,9 @@ export default defineConfig(({ mode }) => {
             },
           ],
         },
+      }),
+      vueI18n({
+        include: resolve(dirname(fileURLToPath(import.meta.url)), '../locales'),
       }),
 
       // Enable https by default
