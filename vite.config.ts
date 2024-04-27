9083d13b253513717862e566b3bcde14fbbce834
kacper jakubek <kjakubek-ulan@taskrabbit.com>
2024-04-27 14:49:18 +0200

    remove i18n

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import basicSSL from "@vitejs/plugin-basic-ssl";
-// import vueI18n from '@intlify/vite-plugin-vue-i18n';
-import { dirname, resolve } from "node:path";
 
 // https://vitejs.dev/config/#conditional-config
 export default defineConfig(({ mode }) => {
