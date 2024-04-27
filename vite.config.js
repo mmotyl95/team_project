673c19e5e65709b574464f729bd48e0e4d2eafd8
kacper jakubek <kjakubek-ulan@taskrabbit.com>
2024-04-27 14:49:16 +0200

    Add app icon and PWA plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    //Plugin for PWA usage
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+
+      // Manifest JSON
+      manifest: {
+        name: "shelbus schedule",
+        short_name: "shelbus",
+        descripion: "Simple bus schedule app",
+        start_url: "/",
+        icons: [
+          {
+            src: "../assets/icons/bus.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
