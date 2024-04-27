7c6b006a06e61be967c2e21fa2f09540ebb78291
SubZero <lukasz.jagnow@gmail.com>
2024-04-27 15:27:13 +0200

    fix plugins\i18n.ts

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -4,10 +4,8 @@ import en from "../locales/en.json";
 const i18n = createI18n({
   legacy: false,
   locale: import.meta.env.VITE_I18N_LOCALE || "en",
-  fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || "zh",
   messages: {
     en,
-    zh,
   },
 });
 
