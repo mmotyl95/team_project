8d889cd6e1902e88a3c6ce65df4c185ef3d906be
kacper jakubek <kjakubek-ulan@taskrabbit.com>
2024-04-27 14:49:15 +0200

    Styling changes made to Timing page.

--- src/views/Timing.vue
+++ src/views/Timing.vue
@@ -1,8 +1,7 @@
 <script setup>
-// const props = defineProps({});
-// const emit = defineEmits;
+import SearchBar from "../components/SearchBar.vue";
 
-async function getBusArrivalData(result) {
+async function getBusArrival(result) {
   /**
    * Get the bus arrival data with the bus stop code.
    * busServices has an object string key value of "services" which contains arrays,
@@ -15,10 +14,29 @@ async function getBusArrivalData(result) {
 
   return busServices;
 }
+getBusArrival();
 </script>
 
 <template>
-  <div>testing testing</div>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a href="" class="navbar-item">
+          <i class="fa fa-arrow-left" aria-hidden="true"></i>
+        </a>
+        <div class="navbar-item">
+          <SearchBar @select-result="getBusArrival(result)" />
+        </div>
+        <a href="" class="navbar-item">
+          <i id="heart" class="far fa-heart" aria-hidden="true"></i>
+        </a>
+      </div>
+    </div>
+  </nav>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+#heart:hover {
+  color: red;
+}
+</style>
